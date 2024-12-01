59a15ab605b462f20cf40520e6b1bb9ec472e66b
wachin <wachin.id@gmail.com>
2024-12-01 14:28:20 -0500

    add  patch to match .NET 9 to Debian 12

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Información general del ensamblado
 [assembly: AssemblyTitle("MIDI_Drumkit_Parser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converts MIDI drum tracks to ASCII tablatures and Sonic Pi code")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tu Empresa")]
 [assembly: AssemblyProduct("MIDI_Drumkit_Parser")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+// Información de versión
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe1b8abe-1d96-4cbd-81a2-0c47943646b0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: Guid("d1f61f2a-2fcd-4a3e-a2d6-5c6a12345678")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
